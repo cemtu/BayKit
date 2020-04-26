11606b4b2093e1d84d34b7f7a15bd085b56e76cd
BayCem <cem_tuncelli@protonmail.ch>
2020-04-26 23:06:50 +0300

    Initial Commit

---
+++ BayKit/BayKit.h
@@ -0,0 +1,19 @@
+//
+//  BayKit.h
+//  BayKit
+//
+//  Created by Cem Tunçelli on 26.04.2020.
+//  Copyright © 2020 Cem Tunçelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BayKit.
+FOUNDATION_EXPORT double BayKitVersionNumber;
+
+//! Project version string for BayKit.
+FOUNDATION_EXPORT const unsigned char BayKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BayKit/PublicHeader.h>
+
+
